5fac7baa52c8e269c14ddfa8f2fd6abca095efbe
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-04-22 20:26:40 +0700

    Use Package bife With (In) R Software

---
+++ bife/bife.R
@@ -0,0 +1,30 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Binary Choice Models with Fixed Effects Use Package bife With (In) R Software
+install.packages("xlsx")
+install.packages("writexl")
+library("xlsx")
+library("writexl")
+library("bife")
+# Import Data Excel Into R From Github Olah Data Semarang (timbulwidodostp)
+github_link <- "https://github.com/timbulwidodostp/bife/raw/main/bife/bife.xlsx"
+temp_file <- tempfile(fileext = ".xlsx")
+req <- GET(github_link, 
+# authenticate using GITHUB_PAT
+authenticate(Sys.getenv("GITHUB_PAT"), ""),
+# write result to disk
+write_disk(path = temp_file))
+bife <- readxl::read_excel(temp_file)
+# Estimate Binary Choice Models with Fixed Effects Use Package bife With (In) R Software
+# Fit a static logit model
+bife <- bife(LFP ~ I(AGE^2) + log(INCH) + KID1 + KID2 + KID3 + factor(TIME) | ID, bife)
+summary(bife)
+# Apply analytical bias correction
+bife_bc <- bias_corr(bife)
+summary(bife_bc)
+# Binary Choice Models with Fixed Effects Use Package bife With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
